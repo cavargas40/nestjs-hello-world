a400a13ed9fc885ff55f316b1f21145b6b31eb99
Carlos Vargas <cavargas40@gmail.com>
2020-05-26 01:33:57 -0500

    transforming Entities to DTOs

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,16 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { LocationService } from './location.service';
-
-interface ILocationListDto {
-  locations: Array<string>;
-}
+import { LocationListDto } from './location-list.dto';
+import { MappingRegistryService } from 'src/common/mapping-registry.service';
+import { LocationDto } from './location.dto';
+import { Location } from './location.entity';
 
 @Controller('location')
 export class LocationController {
-  constructor(private readonly locationService: LocationService) {}
+  constructor(
+    private readonly locationService: LocationService,
+    private readonly mappingRegistryService: MappingRegistryService,
+  ) {}
+
   @Get('list')
-  listLocations(): ILocationListDto {
-    const locations = this.locationService.list();
-    return { locations };
+  public async listLocations(): Promise<LocationListDto> {
+    const locations = await this.locationService.list();
+    const dtos = locations.map(location =>
+      this.mappingRegistryService.map<LocationDto>(
+        Location.name,
+        LocationDto.name,
+        location,
+      ),
+    );
+
+    return { locations: dtos };
   }
 }
